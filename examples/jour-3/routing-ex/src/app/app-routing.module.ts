36198e261bdef7511ad592f66c108831347ac2fb
Thibaut VISTICOT <thibaut.visticot@gmail.com>
2017-01-20 11:23:15 +0100

    routing-ex: route project Init with ng new routing-ex --routing

---
+++ examples/jour-3/routing-ex/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateProductComponent } from '../app/components/create-product/create-product.component';
+
+const routes: Routes = [
+  {
+    path: 'create', 
+    component: CreateProductComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
