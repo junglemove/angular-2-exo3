fd44f703697ec4aeebf7320d3b04cc9eac25101c
Thibaut VISTICOT <thibaut.visticot@gmail.com>
2017-01-20 10:38:18 +0100

    Route Init

--- examples/jour-3/routing/src/app/auth-checker.service.ts
+++ examples/jour-3/routing/src/app/auth-checker.service.ts
@@ -12,6 +12,7 @@ import {
 export class AuthCheckerService implements CanActivate {
 	constructor(private router: Router) {}
 
+	// Auth service used to validate the app.module path: 'admin/:data' route (l.29)
 
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):  boolean {
 		//Check user token in real life
